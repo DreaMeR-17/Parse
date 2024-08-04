b33d69fd0e674c882a7997f4607153cd4589de6f
DreaMeR-17 <pronka_1993@mail.ru>
2024-08-04 16:00:19 +0600

    Добавьте файлы проекта.

---
+++ Parse/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Parse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = ReadInt();
+
+            Console.Write($"Ваше конвертированное число: {value}");
+            Console.ReadKey();
+        }
+
+        static int ReadInt()
+        {
+
+            string userInput;
+            int value = 0;
+
+            bool isUserInputRight = false;
+
+            while (isUserInputRight == false)
+            {
+                Console.Write("Введите число: ");
+                userInput = Console.ReadLine();
+                Console.Clear();
+
+                isUserInputRight = int.TryParse(userInput, out value);
+
+                if (isUserInputRight == false)
+                {
+                    Console.WriteLine("Не удалось конвертировать число, попробуйте еще раз.");
+                }
+            }
+
+            return value;
+        }
+    }
+}
